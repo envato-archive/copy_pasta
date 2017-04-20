3e420a2e3f21dd2d84a5b856146e9276e2003d3c
Justin Mazzi <jmazzi@gmail.com>
2017-04-20 10:12:50 -0400

    Add args

--- extract.rb
+++ extract.rb
@@ -1,15 +1,18 @@
 require 'minitar'
 require 'concurrent'
 
-EXTRACT = Concurrent::FixedThreadPool.new(100, max_queue: 0)
-input   = Archive::Tar::Minitar::Input.new(File.open('testing.tar'))
-errors  = []
-files   = []
+tar_file   = ARGV[0]
+output_dir = ARGV[1]
+
+EXTRACT    = Concurrent::FixedThreadPool.new(100, max_queue: 0)
+input      = Archive::Tar::Minitar::Input.new(File.open(tar_file))
+errors     = []
+files      = []
 
 input.each do |entry|
   if entry.directory?
     # Allow the lib to protect against path attacks
-    input.extract_entry('output', entry)
+    input.extract_entry(output_dir, entry)
   else
     files << [entry.full_name, entry.mode, entry.read]
   end
@@ -18,7 +21,7 @@ end
 files.each do |file|
   EXTRACT.post(file) do |path, mode, content|
     begin
-      path = File.join('output', path)
+      path = File.join(output_dir, path)
 
       File.open(path, 'wb', mode) do |f|
         f.write(content)
@@ -32,8 +35,6 @@ end
 if errors.first
   puts errors.first.message
   puts errors.first.backtrace.join("\n")
-else
-  puts "No errors"
 end
 
 EXTRACT.shutdown
