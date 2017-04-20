81995b681c6fe649bd963c1bcf1c52dab1413b14
Justin Mazzi <jmazzi@gmail.com>
2017-04-20 11:54:37 -0400

    Add benchmarks

--- extract.rb
+++ extract.rb
@@ -1,41 +1,64 @@
 require 'minitar'
 require 'concurrent'
+require 'benchmark'
 
 tar_file   = ARGV[0]
 output_dir = ARGV[1]
 
-EXTRACT    = Concurrent::FixedThreadPool.new(100, max_queue: 0)
-input      = Archive::Tar::Minitar::Input.new(File.open(tar_file))
-errors     = []
-files      = []
+EXTRACT     = Concurrent::FixedThreadPool.new(300, max_queue: 0)
+DIR_CREATE  = Concurrent::FixedThreadPool.new(150, max_queue: 0)
+
+input       = Archive::Tar::Minitar::Input.new(File.open(tar_file))
+errors      = []
+files       = []
+directories = []
 
 input.each do |entry|
   if entry.directory?
-    # Allow the lib to protect against path attacks
-    input.extract_entry(output_dir, entry)
+    # TODO: Path protection
+    directories << entry.full_name
   else
     files << [entry.full_name, entry.mode, entry.read]
   end
 end
 
-files.each do |file|
-  EXTRACT.post(file) do |path, mode, content|
-    begin
-      path = File.join(output_dir, path)
-
-      File.open(path, 'wb', mode) do |f|
-        f.write(content)
+Benchmark.bm do |x|
+  x.report("dirs: ") {
+    directories.each do |dir|
+      DIR_CREATE.post(dir) do |d|
+        begin
+          FileUtils.mkdir_p(File.join(output_dir, d))
+        rescue Exception => e
+          errors << e
+        end
       end
-    rescue Exception => e
-      errors << e
     end
-  end
+
+    DIR_CREATE.shutdown
+    DIR_CREATE.wait_for_termination
+  }
+
+  x.report("files: ") {
+    files.each do |file|
+      EXTRACT.post(file) do |path, mode, content|
+        begin
+          path = File.join(output_dir, path)
+
+          File.open(path, 'wb', mode) do |f|
+            f.write(content)
+          end
+        rescue Exception => e
+          errors << e
+        end
+      end
+    end
+
+    EXTRACT.shutdown
+    EXTRACT.wait_for_termination
+  }
 end
 
 if errors.first
   puts errors.first.message
   puts errors.first.backtrace.join("\n")
 end
-
-EXTRACT.shutdown
-EXTRACT.wait_for_termination
