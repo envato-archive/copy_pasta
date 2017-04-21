ea97d1d93c02ee5db94e3a40c239d4d1b51dd7ea
Justin Mazzi <jmazzi@gmail.com>
2017-04-21 13:49:46 -0400

    Create less IO

--- extract.rb
+++ extract.rb
@@ -9,9 +9,27 @@ EXTRACT     = Concurrent::FixedThreadPool.new(300, max_queue: 0)
 DIR_CREATE  = Concurrent::FixedThreadPool.new(150, max_queue: 0)
 
 input       = Archive::Tar::Minitar::Input.new(File.open(tar_file))
-errors      = []
-files       = []
-directories = []
+errors      = Concurrent::Array.new
+files       = Concurrent::Array.new
+directories = Concurrent::Array.new
+counter     = Concurrent::Array.new
+
+class ParentPaths
+  attr_reader :parents
+
+  def initialize(path)
+    @path    = path.gsub(/\/{1,}+/, '/')
+    @parents = find_parents
+  end
+
+  private
+
+  def find_parents
+    @path.count("/").times.map do
+      @path = File.dirname(@path)
+    end
+  end
+end
 
 input.each do |entry|
   if entry.directory?
@@ -27,7 +45,13 @@ Benchmark.bm do |x|
     directories.each do |dir|
       DIR_CREATE.post(dir) do |d|
         begin
+          paths = ParentPaths.new(d)
           FileUtils.mkdir_p(File.join(output_dir, d))
+
+          paths.parents.each do |parent|
+            directories.delete parent
+          end
+          counter << directories.size
         rescue Exception => e
           errors << e
         end
@@ -62,3 +86,5 @@ if errors.first
   puts errors.first.message
   puts errors.first.backtrace.join("\n")
 end
+
+puts counter.inspect
