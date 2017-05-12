df34032e9f21aa845924d7a7ec6b19ede282e326
Justin Mazzi <jmazzi@gmail.com>
2017-05-12 09:11:05 -0400

    Address the reek issues

---
+++ lib/core_extension/minitar.rb
@@ -0,0 +1,13 @@
+module CoreExtensions
+  # Public: Adds to_hash to Archive::Tar::Minitar::Reader::EntryStream.
+  module MinitarEntryStream
+    # Public: Converts a tar entry to a hash.
+    #
+    # Returns a Hash.
+    def to_hash
+      { file_name: full_name, mode: mode, content: read }
+    end
+  end
+end
+
+Archive::Tar::Minitar::Reader::EntryStream.include(CoreExtensions::MinitarEntryStream)
--- lib/unthread/executor.rb
+++ lib/unthread/executor.rb
@@ -1,17 +1,27 @@
 module Unthread
+  # Public: Executes work across threads.
   class Executor
+    # Public: Array of Concurrent::Promise to execute.
     attr_reader :promises
 
+    # Public: Creates an instance of Executor.
+    #
+    # num_threads - Number of threads to use.
     def initialize(num_threads)
       @thread_pool = Concurrent::FixedThreadPool.new(num_threads)
       @promises    = []
     end
 
+    # Public: Queue work to be done across threads.
+    #
+    # block - Block to be executed when called.
     def queue(&block)
       future = Concurrent::Promise.new(executor: @thread_pool, &block)
       @promises << future.execute
     end
 
+    # Public: Executes all Concurrent::Promises queued and waits for their
+    # return values
     def run
       Concurrent::Promise.zip(*@promises).value!
     end
